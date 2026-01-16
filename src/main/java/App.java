987ef7bc1900f1adcd5ff5e6750fbe3781973691
e-hua <eclipsehua@gmail.com>
2026-01-16 17:35:51 +0800

    Feat: make the App class give out different responses depending on scanned commands

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class App {
 
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
     Eclipse.greet();
+
+    scanLoop: while (true) {
+      String command = scanner.nextLine();
+      switch (command) {
+        case "bye":
+          Eclipse.exit();
+          break scanLoop;
+        default:
+          Eclipse.echo(command);
+      }
+    }
+
+    // Preventing memory leak
+    scanner.close();
   }
 }
