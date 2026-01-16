9caf07bf349212544f45d9bf8031a0fe56a339e4
e-hua <eclipsehua@gmail.com>
2026-01-16 14:55:00 +0800

    Implement the initial skeletal version

--- src/main/java/Eclipse.java
+++ src/main/java/Eclipse.java
@@ -1,10 +1,14 @@
-public class Duke {
+public class Eclipse {
+    public static final String chatbotName = "Eclipse";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
     }
 }
