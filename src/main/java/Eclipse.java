6bfae6d11e7eed84d1737f4d84d2aef6db751239
e-hua <eclipsehua@gmail.com>
2026-01-16 16:30:26 +0800

    Feat: add greet method to Eclipse chatbot

--- src/main/java/Eclipse.java
+++ src/main/java/Eclipse.java
@@ -1,2 +1,16 @@
 public class Eclipse {
+    public static final String name = "Eclipse";
+
+    public Eclipse() {
+
+    }
+
+    public static void greet() {
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm " + Eclipse.name);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+    }
 }
