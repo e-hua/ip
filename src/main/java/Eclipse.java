b7b651474cce4634fb0271d3c9d08b149175ca50
e-hua <eclipsehua@gmail.com>
2026-01-16 15:54:02 +0800

    Chore: extract App class from Eclipse.java as application entry

--- src/main/java/Eclipse.java
+++ src/main/java/Eclipse.java
@@ -1,14 +1,2 @@
 public class Eclipse {
-    public static final String chatbotName = "Eclipse";
-
-    public static void main(String[] args) {
-        String horizontalLine = "____________________________________________________________";
-
-        System.out.println(horizontalLine);
-        System.out.println("Hello! I'm " + chatbotName);
-        System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
-    }
 }
