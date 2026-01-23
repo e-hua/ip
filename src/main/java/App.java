aab3431a466fff5993a3821b29d91dffb6c389ec
e-hua <eclipsehua@gmail.com>
2026-01-23 23:02:20 +0800

    Feat: update the App class to use the Storage feature

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,28 @@
 import Exceptions.EclipseException;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
+        Storage tasksStorage;
+        Eclipse chatbot;
+        String dirPath = "./data";
+
+        try {
+            tasksStorage = new Storage(new StorageParser(), dirPath);
+            chatbot = new Eclipse(tasksStorage.readTasks());
+        } catch (EclipseException e) {
+            Eclipse.printIndentedHorizontalLine();
+            Eclipse.printIndentedLine("Fatal, chatbot failed to boot!!! " + e.getMessage());
+            Eclipse.printIndentedHorizontalLine();
+            return;
+        }
+
         Scanner scanner = new Scanner(System.in);
 
         Eclipse.greet();
@@ -12,30 +30,34 @@ public class App {
         scanLoop:
         while (true) {
             String input = scanner.nextLine();
-            ParsedInput parsedInput = Parser.parse(input);
+            ParsedInput parsedInput = Parser.parse(input, chatbot);
 
             try {
                 switch (parsedInput.getCommand()) {
-                    case BYE:
-                        Eclipse.exit();
-                        break scanLoop;
-                    case LIST:
-                        Eclipse.list();
-                        break;
-                    case MARK:
-                        Eclipse.mark(Parser.parseListIndex(parsedInput.getParams()));
-                        break;
-                    case UNMARK:
-                        Eclipse.unmark(Parser.parseListIndex(parsedInput.getParams()));
-                        break;
-                    case EVENT, DEADLINE, TODO:
-                        Eclipse.add(parsedInput);
-                        break;
-                    case DELETE:
-                        Eclipse.delete(Parser.parseListIndex(parsedInput.getParams()));
-                        break;
-                    default:
-                        throw new EclipseException("Unknown input: " + input);
+                case BYE:
+                    Eclipse.exit();
+                    break scanLoop;
+                case LIST:
+                    chatbot.list();
+                    break;
+                case MARK:
+                    chatbot.mark(Parser.parseListIndex(parsedInput.getParams(), chatbot));
+                    tasksStorage.storeTasks(chatbot.getTasks());
+                    break;
+                case UNMARK:
+                    chatbot.unmark(Parser.parseListIndex(parsedInput.getParams(), chatbot));
+                    tasksStorage.storeTasks(chatbot.getTasks());
+                    break;
+                case EVENT, DEADLINE, TODO:
+                    chatbot.add(parsedInput);
+                    tasksStorage.storeTasks(chatbot.getTasks());
+                    break;
+                case DELETE:
+                    chatbot.delete(Parser.parseListIndex(parsedInput.getParams(), chatbot));
+                    tasksStorage.storeTasks(chatbot.getTasks());
+                    break;
+                default:
+                    throw new EclipseException("Unknown input: " + input);
                 }
             } catch (EclipseException e) {
                 Eclipse.printIndentedHorizontalLine();
